3e104290f1b7078d716eee94ee2681ce6b44e20c
mjjsingh <jyoti1536@gmail.com>
2024-01-10 19:03:01 +0530

    Request handler

--- server.js
+++ server.js
@@ -1,30 +1,51 @@
-const express = require('express');
 const fs = require('fs');
-const app = express();
-const port = 3000;
 
-// Middleware to parse the body of the request
-app.use(express.urlencoded({ extended: true }));
+const requestHandler = (req, res) => {
+  const url = req.url;
+  const method = req.method;
+  if (url === '/') {
+    res.write('<html>');
+    res.write('<head><title>Enter Message</title></head>');
+    res.write(
+      '<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>'
+    );
+    res.write('</html>');
+    return res.end();
+  }
+  
+  if (url === '/message' && method === 'POST') {
+    const body = [];
+    req.on('data', chunk => {
+      console.log(chunk);
+      body.push(chunk);
+    });
+    return req.on('end', () => {
+      const parsedBody = Buffer.concat(body).toString();
+      const message = parsedBody.split('=')[1];
+      fs.writeFile('message.txt', message, err => {
+        res.statusCode = 302;
+        res.setHeader('Location', '/');
+        return res.end();
+      });
+    });
+  }
+  res.setHeader('Content-Type', 'text/html');
+  res.write('<html>');
+  res.write('<head><title>My First Page</title><head>');
+  res.write('<body><h1>Hello from my Node.js Server!</h1></body>');
+  res.write('</html>');
+  res.end();
+};
 
-// Route to display the form
-app.get('/', (req, res) => {
-  res.send(`
-    <form action="/submit" method="POST">
-      <input type="text" name="message" placeholder="Write something..." required>
-      <button type="submit">Submit</button>
-    </form>
-  `);
-});
+// module.exports = requestHandler;
 
-// Route to handle form submission
-app.post('/submit', (req, res) => {
-  const message = req.body.message;
+// module.exports = {
+//     handler: requestHandler,
+//     someText: 'Some hard coded text'
+// };
 
-  // Write the message to a file
-  fs.writeFile('message.txt', message, (err) => {
-    if (err) throw err;
+// module.exports.handler = requestHandler;
+// module.exports.someText = 'Some text';
 
-  });
-});
-
-app.listen(port);
+exports.handler = requestHandler;
+exports.someText = 'Some hard coded text';
